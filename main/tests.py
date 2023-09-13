082295081c94eb8b9797954029b69527003d32c3
odracheer <akunsekolahricardo@gmail.com>
2023-09-13 11:52:44 +0700

    Bonus Test

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,5 @@
-from django.test import TestCase
-
 from django.test import TestCase, Client
+from .models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -10,3 +9,19 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def setUp(self):
+        self.tesObj = Item.objects.create(
+            name = "Arabica",
+            price = 50000,
+            amount = 100,
+            description = "Kopi Arabica: Rasakan Keajaiban Rasa dalam Setiap Titis."
+        )
+    
+    def test_model_method(self):
+        obj = Item.objects.get(id=self.tesObj.id)
+        self.assertEqual(obj.name, "Arabica")
+        self.assertEqual(obj.price, 50000)
+        self.assertEqual(obj.amount, 100)
+        self.assertEqual(obj.description, "Kopi Arabica: Rasakan Keajaiban Rasa dalam Setiap Titis."
+        )
