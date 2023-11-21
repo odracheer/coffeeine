a97a5a620a8a5d987e97d2178884db4202a9d0cc
odracheer <akunsekolahricardo@gmail.com>
2023-11-21 20:12:54 +0700

    Completing connection to flutter

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, \
                        register, login_user, logout_user, edit_item, delete_item, get_item_json, add_item_ajax, \
-                       increment_item_ajax, decrement_item_ajax, delete_item_ajax
+                       increment_item_ajax, decrement_item_ajax, delete_item_ajax, create_item_flutter
 from . import views
 
 app_name = 'main'
@@ -23,4 +23,5 @@ urlpatterns = [
      path('increment-item-ajax/<int:id>/', increment_item_ajax, name='increment_item_ajax'),
      path('decrement-item-ajax/<int:id>/', decrement_item_ajax, name='decrement_item_ajax'),
      path('delete-item-ajax/<int:id>/', delete_item_ajax, name='delete_item_ajax'),
+     path('create-flutter/', create_item_flutter, name='create_item_flutter'),
 ]
